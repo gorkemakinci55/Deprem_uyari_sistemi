b18bb10aabeabedf13be8f47a614e1d4efecb470
gorkem <gorkem.akinci55@gmail.com>
2020-04-20 22:02:28 +0300

    bma_400 init config has been done

---
+++ Debug/Core/Inc/bma400.d
@@ -0,0 +1,6 @@
+Core/Inc/bma400.o: ../Core/Inc/bma400.c ../Core/Inc/bma400.h \
+ ../Core/Inc/bma400_defs.h
+
+../Core/Inc/bma400.h:
+
+../Core/Inc/bma400_defs.h:
---
+++ Release/Core/Inc/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Inc/bma400.c 
+
+OBJS += \
+./Core/Inc/bma400.o 
+
+C_DEPS += \
+./Core/Inc/bma400.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Inc/bma400.o: ../Core/Inc/bma400.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -DUSE_HAL_DRIVER -DSTM32L071xx -c -I../Drivers/CMSIS/Include -I../Core/Inc -I../Drivers/STM32L0xx_HAL_Driver/Inc -I../Drivers/CMSIS/Device/ST/STM32L0xx/Include -I../Drivers/STM32L0xx_HAL_Driver/Inc/Legacy -Os -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Core/Inc/bma400.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
